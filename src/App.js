0a3d49373e5eb62a089023e5dff791b58edbb67f
Syed Khalid <magmastar167@gmail.com>
2023-10-12 11:57:04 +0300

    🌛

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function ColorPicker({ colors }) {
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [showColorList, setShowColorList] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowColorList(!showColorList);
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
+    setShowColorList(false);
+  };
+
+  return (
+    <div className="color-picker">
+      <button onClick={handleButtonClick}>Pick a color</button>
+      {showColorList && (
+        <ul className="color-list">
+          {colors.map((color, index) => (
+            <li
+              key={index}
+              onClick={() => handleColorClick(color)}
+              style={{ backgroundColor: color }}
+            ></li>
+          ))}
+        </ul>
+      )}
+      {selectedColor && (
+        <p>Selected Color: <span style={{ backgroundColor: selectedColor }}>{selectedColor}</span></p>
+      )}
+    </div>
+  );
+}
+
 function App() {
+  const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color Picker App</h1>
+      <ColorPicker colors={colors} />
     </div>
   );
 }
 
 export default App;
+
